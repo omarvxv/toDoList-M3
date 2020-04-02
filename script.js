e96f41019662da51980d56869b637e1d15c81dea
Omar Valikhanov <omar.velikhanov@gmail.com>
2020-04-03 00:34:02 +0400

    submit

--- script.js
+++ script.js
@@ -8,21 +8,21 @@ class ManageList {
 
         this.taskListBlock = document.querySelector('.list');
         this.listOfTasks = this.taskListBlock.children;
-        this.ELEMENT_HEIGHT = 40;
-        this.LIST_BORDER = 2;
+        this.ELEMENT_HEIGHT = 40;  // высота элемента списка
+        this.LIST_BORDER = 2;  // сумма границ списка (top + bottom 1px)
         this.sortIcon = document.querySelector('.sort');
         this.sortIcon.addEventListener('click', this.sortList);
         this.sorted = false;
     }
 
     addTask = () => {
-        const task = new Task(this.taskListBlock);
+        const task = new Task(this.taskListBlock);  // создаём объект который содержит в себе записанные данные на странице
         this.value = task.value;
         this.taskListBlock.append(this.generateTaskLine(this.value));
-        this.taskListBlock.lastElementChild.querySelector('.task').focus();
+        this.taskListBlock.lastElementChild.querySelector('.task').focus();  // фокусируемся на последнем элементе
     }
 
-    generateTaskLine(value) {
+    generateTaskLine(value) {  
         const taskBlock = document.createElement('div');
         const dragPoint = document.createElement('div');
         const inputLine = document.createElement('input');
@@ -36,7 +36,7 @@ class ManageList {
         cancelTask.classList.add('cancel-task');
 
         dragPoint.addEventListener('mousedown', this.moveTaskLine);
-        cancelTask.addEventListener('click', (event) => this.deleteTaskLine(event));
+        cancelTask.addEventListener('click', event => this.deleteTaskLine(event));
 
         taskBlock.append(dragPoint);
         taskBlock.append(inputLine);
@@ -47,9 +47,9 @@ class ManageList {
 
     sortList = () => {
         this.sortIcon.classList.toggle('sorted');
-        const arrayOfTasks = [].map.call(this.listOfTasks, element => element);
-        let sortedArray = arrayOfTasks.sort(callBackSort);
-        function callBackSort(a, b) {
+        const arrayOfTasks = [].map.call(this.listOfTasks, element => element);  // массив списка находящегося на странице
+        let sortedArray = arrayOfTasks.sort(callBackSort);  // отсортированный список
+        function callBackSort(a, b) {   // колбэк функция для правильной сортировки элементов
             a=a.children[1].value
             b=b.children[1].value
             if(!isNaN(a) && !isNaN(b)){
@@ -64,7 +64,7 @@ class ManageList {
         }
         const sortedList = document.createElement('div');
         sortedList.classList.add('list');
-        sortedArray.forEach(element => sortedList.append(element));
+        sortedArray.forEach(element => sortedList.append(element));  // вставляется в лист отсортированный список
         this.taskListBlock.replaceWith(sortedList);
         this.taskListBlock = sortedList;
         this.listOfTasks = this.taskListBlock.children;
@@ -84,15 +84,15 @@ class ManageList {
         document.addEventListener('mousemove', this.handleMouseMove);
         document.addEventListener('mouseup', this.dropElement);
 
-        this.movedElement = e.target.parentNode;
-        const elementBorder = this.movedElement.getBoundingClientRect();
-        const listBorder = this.taskListBlock.getBoundingClientRect();
-        this.y = e.pageY - elementBorder.top + listBorder.top;
-
+        this.movedElement = e.target.parentNode;  // перетаскиваемый элемент
+        const elementBorder = this.movedElement.getBoundingClientRect();  // границы передвигающегося элемента
+        const listBorder = this.taskListBlock.getBoundingClientRect();  // границы списка (родитель)
+        this.y = e.pageY - elementBorder.top + listBorder.top; // расстояние от верхней точки окна до приблизительно центра элемента
+                                                               // касающегося верхней точки своего родителя
         this.movedElement.style = 'background: #FFDC40; position: absolute;';
-        this.listAfterClick = this.taskListBlock.getBoundingClientRect();
-        this.listHeight = this.listAfterClick.bottom - this.listAfterClick.top - this.LIST_BORDER;
-        let top = 0;
+        this.listAfterClick = this.taskListBlock.getBoundingClientRect();  // границы списка после вытаскивания элемента из него
+        this.listHeight = this.listAfterClick.bottom - this.listAfterClick.top - this.LIST_BORDER;  // высота изменённого спика
+        let top = 0;  // расстояние от верха родителя до перетаскиваемого элемента
         if (elementBorder.y - this.listAfterClick.y < 0) {
 
         } else if (elementBorder.bottom - this.listAfterClick.y > this.listHeight) {
@@ -104,22 +104,22 @@ class ManageList {
     }
     handleMouseMove = (e) => {
         this.taskListBlock.style.cursor = 'move';
-        const elemCenter = this.ELEMENT_HEIGHT / 2;
+        const elemCenter = this.ELEMENT_HEIGHT / 2; // центр движущегося элемента
         if (e.pageY - this.y >= elemCenter && e.pageY <= this.listAfterClick.bottom - elemCenter) {
-            this.movedElement.style.top = (e.pageY - this.y - elemCenter) + 'px';
+            this.movedElement.style.top = (e.pageY - this.y - elemCenter) + 'px';  // движение элемента по оси Y
         }
     }
     dropElement = () => {
         document.removeEventListener('mouseup', this.dropElement);
         document.removeEventListener('mousemove', this.handleMouseMove);
 
-        let movedElementClone = this.taskListBlock.removeChild(this.movedElement);
-        let position = Math.round(movedElementClone.style.top.slice(0, -2) / this.ELEMENT_HEIGHT);
+        let movedElementClone = this.taskListBlock.removeChild(this.movedElement);  // клон вставляемого элемента
+        let position = Math.round(movedElementClone.style.top.slice(0, -2) / this.ELEMENT_HEIGHT);  // позиция элемента
 
-        let inPosition = this.taskListBlock.children[position];
-        let ratio = this.movedElement.style.top.slice(0, -2) / this.ELEMENT_HEIGHT;
-        movedElementClone.style = '';
-        const side = ratio < 0.5 ? 'beforebegin' : 'afterend';
+        let inPosition = this.taskListBlock.children[position];  // элемент находящийся на этой позиции
+        let ratio = this.movedElement.style.top.slice(0, -2) / this.ELEMENT_HEIGHT;  // склонность . определяется куда будет вставлен
+        movedElementClone.style = '';                                               // элемент относительно элемента находящегося 
+        const side = ratio < 0.5 ? 'beforebegin' : 'afterend';                      // на этой позиции
 
         inPosition.insertAdjacentElement(side, movedElementClone);
     }
